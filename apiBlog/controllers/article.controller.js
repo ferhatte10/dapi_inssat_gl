57b59b38f71c3af08fdda4dcf78563701ccc9d9a
antoineMaurais <antoine.maurais98@gmail.com>
2023-11-02 18:10:12 +0100

    Ajout endpoints filtre article

--- apiBlog/controllers/article.controller.js
+++ apiBlog/controllers/article.controller.js
@@ -1,10 +1,11 @@
 const {
-  article : ArticleModel,
-  user : UserModel,
-  tag : TagModel,
-  article_tag : Article_tagModel,
-  category : CategoryModel
-} = require('../configs/db/config/db'); 
+  article: ArticleModel,
+  user: UserModel,
+  tag: TagModel,
+  article_tag: Article_tagModel,
+  category: CategoryModel,
+  Sequelize
+} = require('../configs/db/config/db');
 
 const ArticleController = {};
 
@@ -151,7 +152,136 @@ ArticleController.getArticlesWithDetails = async (req, res) => {
   }
 };
 
+// Get articles by category
+ArticleController.getArticlesByCategory = async (req, res) => {
+  const categoryId = parseInt(req.params.categoryId);
 
+  try {
+    // Recherchez la catégorie par son ID
+    const category = await CategoryModel.findByPk(categoryId);
+
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    // Recherchez les articles associés à la catégorie
+    const articles = await ArticleModel.findAll({
+      where: { category_id: categoryId },
+    });
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+// Get articles by tag
+ArticleController.getArticlesByTag = async (req, res) => {
+  const tagId = parseInt(req.params.tagId);
+
+  try {
+    // Recherchez le tag par son ID
+    const tag = await TagModel.findByPk(tagId);
+
+    if (!tag) {
+      return res.status(404).json({ error: 'Tag not found' });
+    }
+
+    // Recherchez les articles associés à ce tag en utilisant la table de liaison article_tag
+    const articles = await ArticleModel.findAll({
+      include: [
+        {
+          model: Article_tagModel,
+          as: 'article_tags',
+          where: { tag_id: tagId },
+        },
+      ],
+    });
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+// Get articles by multiple tags
+ArticleController.getArticlesByTags = async (req, res) => {
+  const tagIds = req.query.tagIds;
+
+  try {
+    // Recherchez les tags correspondants par leurs IDs
+    const tags = await TagModel.findAll({
+      where: { id: tagIds },
+    });
+
+    if (!tags || tags.length === 0) {
+      return res.status(404).json({ error: 'Tags not found' });
+    }
+
+    // Recherchez les articles associés à ces tags en utilisant la table de liaison article_tag
+    const articles = await ArticleModel.findAll({
+      include: [
+        {
+          model: ArticleTagModel,
+          as: 'article_tags',
+          where: {
+            tag_id: {
+              [Sequelize.Op.in]: tagIds,
+            },
+          },
+        },
+      ],
+    });
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+// Get articles by author
+ArticleController.getArticlesByAuthor = async (req, res) => {
+  const authorId = parseInt(req.params.authorId);
+
+  try {
+    // Recherchez les articles de l'auteur spécifié
+    const articles = await ArticleModel.findAll({
+      where: { author_id: authorId },
+    });
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Get articles by time period
+ArticleController.getArticlesByTimePeriod = async (req, res) => {
+  const dateStart = new Date(req.query.dateStart);
+  const dateEnd = new Date(req.query.dateEnd);
+
+  try {
+    // Recherchez les articles publiés dans la période de temps spécifiée
+    const articles = await ArticleModel.findAll({
+      where: {
+        published_at: {
+          [Sequelize.Op.between]: [dateStart, dateEnd],
+        },
+      },
+    });
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 // TODO: Add More controllers methods to manage the additional routes 
 
--- apiBlog/routes/article.route.js
+++ apiBlog/routes/article.route.js
@@ -12,6 +12,24 @@ router.put('/:id', ArticleController.update);
 // Additional routes
 // Retrieve a list of articles with extended details including author info and tags title
 router.get('/details', ArticleController.getArticlesWithDetails);
+
+// Retrieve a list of articles by category
+router.get('/categories/:categoryId', ArticleController.getArticlesByCategory);
+
+// Retrieve a list of articles by tag
+router.get('/tag/:tagId', ArticleController.getArticlesByTag);
+
+// Retrieve a list of articles by multiple tags
+router.get('/tags/:tagIds', ArticleController.getArticlesByTags);
+
+// Retrieve a list of articles by authorId
+router.get('/author/:authorId', ArticleController.getArticlesByAuthor);
+
+// Retrieve a list of articles by time-period
+router.get('/time-period', ArticleController.getArticlesByTimePeriod);
+// exemple de requete = GET /articles/time-period?dateStart=2023-01-01&dateEnd=2023-12-31
+
+
 // TODO: 
 
 module.exports = router;
