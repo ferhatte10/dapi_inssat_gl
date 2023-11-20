7af03d67ec554dcbba502a553c8eaa134fa0c2f1
Ferhat SAIDOUN <ferhatte10@gmail.com>
2023-11-20 04:02:39 +0100

    gateway proxy added path to enssat ade and blogs

--- gateway/config/routes.js
+++ gateway/config/routes.js
@@ -20,6 +20,50 @@ exports.ROUTES = [
                 //[`^/api_blog`]: '',
             },
         }
+    },
+    {
+        url: '/blogs_enssat',
+        auth: false,
+        // rateLimit: {
+        //     windowMs: 15 * 60 * 1000,
+        //     max: 10
+        // },
+        proxy: {
+            target: "http://blog.enssat.fr/feeds/posts/default",
+            changeOrigin: true,
+            headers: {
+                "Connection": "keep-alive"
+            },
+            onProxyRes: function (proxyRes, req, res) {
+                proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+            },
+            // followRedirects: true,
+            pathRewrite: {
+                [`^/blogs_enssat`]: '',
+            },
+        }
+    },
+    {
+        url: '/ade',
+        auth: false,
+        // rateLimit: {
+        //     windowMs: 15 * 60 * 1000,
+        //     max: 10
+        // },
+        proxy: {
+            target: "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings",
+            changeOrigin: true,
+            headers: {
+                "Connection": "keep-alive"
+            },
+            onProxyRes: function (proxyRes, req, res) {
+                proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+            },
+            // followRedirects: true,
+            pathRewrite: {
+                [`^/ade`]: '',
+            },
+        }
     }
     
 ]
